8f26c9f63c3830c14f8a22b4e71d82cb9d2abf09
ht1423 <tharshita0215@gmail.com>
2025-08-12 10:38:42 +0530

    final

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -5,7 +5,7 @@ import Gradient from '../assets/ContactAsset/gradient.svg'
 
 function Contact() {
   return (
-    <section className="relative mt-[156px] w-full flex justify-center items-center pb-0 sm:pb-12 md:pb-24">
+    <section className="relative pt-24 w-full flex justify-center items-center  sm:pb-12 pb-8 md:pb-24">
       
       {/* Gradient background */}
       <img src={Gradient} alt=""  className="absolute z-0 w-full max-w-[3004px] max-h-[600px py-24 sm:max-h-[800px] md:max-h-[1000px] lg:max-h-[1228px] left-0 opacity-100 "/>
