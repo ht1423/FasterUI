e30688083b94b952f70972c66fde839cbe622728
ht1423 <tharshita0215@gmail.com>
2025-08-14 01:21:52 +0530

    this one's final promise

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -5,13 +5,13 @@ import Gradient from '../assets/ContactAsset/gradient.svg'
 
 function Contact() {
   return (
-    <section className="relative pt-24 w-full flex justify-center items-center  sm:pb-12 pb-8 md:pb-24">
+    <section className="relative pt-28 lg:pt-40 w-full flex justify-center items-center sm:pb-12 pb-8 md:pb-24">
       
       {/* Gradient background */}
       <img src={Gradient} alt=""  className="absolute z-0 w-full h-[1228px] left-0 opacity-100 mt-[-270px] md:mt-[-250px] lg:mt-[-320px] xl:mt-[-350px]"/>
 
       {/* Wheel Background Image */}
-      <div className="absolute w-[400px] h-[250px] sm:w-[400px] md:w-[600px] md:h-[300px] lg:w-[800px] lg:h-[400px] xl:w-[998px] xl:h-[525px]  left-0 opacity-100 mix-blend-color-burn pointer-events-none bg-no-repeat bg-contain top-[-50px] md:top-[-120px] lg:top-[-170px] xl:top-[-350px]"
+      <div className="absolute w-[400px] h-[250px] sm:w-[400px] md:w-[600px] md:h-[300px] lg:w-[800px] lg:h-[400px] xl:w-[998px] xl:h-[525px]  left-0 opacity-100 mix-blend-color-burn pointer-events-none bg-no-repeat bg-contain top-[-50px] md:top-[-120px] lg:top-[-170px] xl:top-[-320px]"
 
         style={{
           backgroundImage: `url(${Wheel})`,
